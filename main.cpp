1a22a473b8940347793ed9796832d57e9d26cb61
Marc Juneau <marc.juneau@clg.qc.ca>
2025-03-21 16:43:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,34 @@
+/**
+  * @file       main.cpp
+  * @brief      Programme principal du projet
+  * @author     Marc Juneau (marcjuneau@gmail.com)
+  * @version    1.0
+  * @date       21 mars 2025
+  *
+  * @mainpage
+  *  Projet Exemple
+  *	@author Marc Juneau
+  *	@section MainSection1 Description
+  *
+  * Ce programme affiche un message sur la console et attend une entrée de l'utilisateur.
+  */
+
 #include <iostream>
 #include <Windows.h>
 
 using namespace std;
 
+// Constantes pour les valeurs numériques
+#define CODE_PAGE_UTF8 1252
+
+/**
+  * @brief      Fonction principale du programme
+  * @return     Code de sortie du programme
+  */
 int main()
 {
-    SetConsoleOutputCP(1252);
-    cout << "Vive l'�t�!" << endl << flush;
+    SetConsoleOutputCP(CODE_PAGE_UTF8);
+    cout << "Vive l'été!" << endl << flush;
     cin.get();
     return 0;
 }
